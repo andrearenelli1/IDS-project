f6831e48512710124fa5841cb1b2fbba9a9bb7cb
andrearenelli1 <andrea.renelli@studenti.unitn.it>
2026-06-24 01:02:04 +0200

    new results

--- run_experiments.py
+++ run_experiments.py
@@ -75,7 +75,7 @@ AREA_SIZES = [100, 200]        # [m]  lato workspace
 N_DRONES_LIST = [3, 4, 5]          # numero di agenti
 
 # Numero di posizioni vittima casuali: ognuna campionata indipendentemente in run_one().
-N_RANDOM_VICTIMS = 20
+N_RANDOM_VICTIMS = 2
 
 ARTVA_NOISE_STDS = [1e-7, 1e-6, 1e-5]  # rumore segnale ARTVA
 
